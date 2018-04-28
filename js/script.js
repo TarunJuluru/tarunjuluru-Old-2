57f107bb2a17b4595ec9ed8f63b8ae8134c148b8
Tarun Juluru <tarun.design99@gmail.com>
2018-04-28 16:18:56 +0530

    Added interactions to Navigation using jQuery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+/*global document*/
+/*global window*/
+/*global $*/
+/*$ = jQuery*/
 
+$(document).ready(function () {
+    "use strict";
+
+    var scrollLink = $('.nav-link');
+
+    // Smooth scrolling
+    scrollLink.click(function (event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top - 140
+        }, 1000);
+    });
+
+    // Active link switching
+    $(window).scroll(function () {
+
+        scrollLink.on('click', function (event) {
+            $(event.currentTarget).parent().addClass('active');
+            $(event.currentTarget).parent().siblings().removeClass('active');
+        });
+    });
+
+    //collapsing nav bar when clicked on nav links
+    scrollLink.on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+});
