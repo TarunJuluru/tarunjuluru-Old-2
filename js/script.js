2cc1c9f2652ae538c87c177cc4f57a9b14bfb23a
Tarun Juluru <tarun.design99@gmail.com>
2018-05-25 19:48:29 +0530

    version-2 formated

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 /*global $*/
 /*$ = jQuery*/
 
-$(document).ready(function () {
+$(document).ready(function() {
     "use strict";
 
     var $scrollLink = $('.nav-link');
@@ -27,7 +27,7 @@ $(document).ready(function () {
 
 
     // Smooth scrolling through respective sections on clicking nav-links
-    $scrollLink.click(function (event) {
+    $scrollLink.click(function(event) {
         event.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top - 140
@@ -35,11 +35,11 @@ $(document).ready(function () {
     });
 
     // Active link switching
-    $(window).scroll(function () {
+    $(window).scroll(function() {
 
         var scrollbarLocation = $(this).scrollTop();
 
-        $scrollLink.each(function () {
+        $scrollLink.each(function() {
 
             var sectionOffset = $(this.hash).offset().top - 160;
 
@@ -50,7 +50,7 @@ $(document).ready(function () {
         });
 
         //scroll animation on click
-        $backToTop.on("click", function () {
+        $backToTop.on("click", function() {
             $("html, body").stop().animate({
                 scrollTop: 0
             }, 2000);
@@ -60,7 +60,7 @@ $(document).ready(function () {
     });
 
     //collapsing nav bar when clicked on nav links
-    $scrollLink.on('click', function () {
+    $scrollLink.on('click', function() {
         $('.navbar-collapse').collapse('hide');
     });
 
