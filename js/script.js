3d39593b997d0b49d4bec3ee58dc04c9163e9d6b
Tarun Juluru <tarun.design99@gmail.com>
2018-05-25 19:42:52 +0530

    Personal Portfolio version-2

--- js/script.js
+++ js/script.js
@@ -3,14 +3,31 @@
 /*global $*/
 /*$ = jQuery*/
 
-$(document).ready(function() {
+$(document).ready(function () {
     "use strict";
 
-    var scrollLink = $('.nav-link');
+    var $scrollLink = $('.nav-link');
     var $backToTop = $(".back-to-top");
 
+    //    var $menuOpen = $(".menu-icon");
+    //    var $menuClose = $(".close-icon");
+    //
+    //    //hamburgerMenu toogle onClick
+    //    $menuClose.hide();
+    //
+    //    $menuOpen.onclick(function () {
+    //        $menuOpen.hide();
+    //        $menuClose.show();
+    //    });
+    //
+    //    $menuClose.onclick(function () {
+    //        $menuOpen.show();
+    //        $menuClose.hide();
+    //    });
+
+
     // Smooth scrolling through respective sections on clicking nav-links
-    scrollLink.click(function(event) {
+    $scrollLink.click(function (event) {
         event.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top - 140
@@ -18,11 +35,11 @@ $(document).ready(function() {
     });
 
     // Active link switching
-    $(window).scroll(function() {
+    $(window).scroll(function () {
 
         var scrollbarLocation = $(this).scrollTop();
 
-        scrollLink.each(function() {
+        $scrollLink.each(function () {
 
             var sectionOffset = $(this.hash).offset().top - 160;
 
@@ -33,7 +50,7 @@ $(document).ready(function() {
         });
 
         //scroll animation on click
-        $backToTop.on("click", function() {
+        $backToTop.on("click", function () {
             $("html, body").stop().animate({
                 scrollTop: 0
             }, 2000);
@@ -43,7 +60,7 @@ $(document).ready(function() {
     });
 
     //collapsing nav bar when clicked on nav links
-    scrollLink.on('click', function() {
+    $scrollLink.on('click', function () {
         $('.navbar-collapse').collapse('hide');
     });
 
